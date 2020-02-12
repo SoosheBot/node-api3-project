9821b82b4272eac0b67e9d88026d37ee50ec845a
sooshe <sooshe@sooshebot.com>
2020-02-12 16:40:26 -0600

    get all users success

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
 
 const server = express();
+server.use(cors());
+
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,49 @@
-const express = require('express');
+const express = require("express");
+
+const Users = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  // do your magic!
+  const users = req.body;
+  Users.insert(users)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "Could not post to users" });
+    });
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  Users.get(req.query)
+  .then(user => {
+    res.status(201).json(user);
+  })
+  .catch(err => {
+    res.status(500).json({errorMessage: "Could not get users."})
+  })
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
