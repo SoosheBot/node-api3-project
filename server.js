98f70f2e587ae229d08ddd3e645f24ed3e30b45a
sooshe <sooshe@sooshebot.com>
2020-02-13 11:27:37 -0600

    add custom middleware to server file

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const postRouter = require("./posts/postRouter");
 const userRouter = require("./users/userRouter");
 
 const server = express();
-server.use(cors());
 
-server.use("/api/posts", postRouter);
-server.use("/api/users", userRouter);
+//custom middleware
+function logger(req,res,next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}
+    )}`
+  );
+  next();
+};
+
+server.use(cors());
+server.use(helmet())
+
+server.use("/api/posts", logger, postRouter);
+server.use("/api/users", logger, userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
